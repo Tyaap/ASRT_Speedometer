3dae8978a17d4a2a747f0529c4efa45c21f2becd
Tyaap <48681812+Tyaap@users.noreply.github.com>
2019-03-19 21:30:42 +0000

    Add files via upload

--- Speedo/Hook/FunctionOrdinals.cs
+++ Speedo/Hook/FunctionOrdinals.cs
@@ -122,23 +122,4 @@
         DeletePatch = 117,
         CreateQuery = 118,
     }
-
-    public enum Direct3DDevice9ExFunctionOrdinals : short
-    {
-        SetConvolutionMonoKernel = 119,
-        ComposeRects = 120,
-        PresentEx = 121,
-        GetGPUThreadPriority = 122,
-        SetGPUThreadPriority = 123,
-        WaitForVBlank = 124,
-        CheckResourceResidency = 125,
-        SetMaximumFrameLatency = 126,
-        GetMaximumFrameLatency = 127,
-        CheckDeviceState_ = 128,
-        CreateRenderTargetEx = 129,
-        CreateOffscreenPlainSurfaceEx = 130,
-        CreateDepthStencilSurfaceEx = 131,
-        ResetEx = 132,
-        GetDisplayModeEx = 133,
-    }
 }
