08add707e2d3e140348d474aeb77f62fbc175a13
Tyaap <alex.liddiard@hotmail.co.uk>
2021-05-25 19:26:51 +0100

    Updated EXE metadata.

--- Speedo Loader/Properties/AssemblyInfo.cs
+++ Speedo Loader/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Speedo Loader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Speedometer")]
+[assembly: AssemblyDescription("A speedometer for Sonic & All-Stars Racing Transformed.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Tyapp")]
+[assembly: AssemblyProduct("Speedometer")]
+[assembly: AssemblyCopyright("Tyapp")]
+[assembly: AssemblyTrademark("Tyapp")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
